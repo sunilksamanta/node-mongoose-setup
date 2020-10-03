ee233bc2b0bd9b9c4d45abc57407969c9cb0cd67
Sunil Kumar Samanta <sunilthedj@gmail.com>
2020-10-03 10:28:25 +0530

    Mongo Error Handling fix

--- config/database.js
+++ config/database.js
@@ -3,15 +3,24 @@ const config = require('./config').getConfig();
 class Connection {
   constructor() {
     const url = config.MONGO_URL;
-    console.log("Establish new connection with url", url);
     mongoose.Promise = global.Promise;
     mongoose.set("useNewUrlParser", true);
     mongoose.set("useFindAndModify", false);
     mongoose.set("useCreateIndex", true);
     mongoose.set("useUnifiedTopology", true);
-    mongoose.connect(url, err => {
-      console.error('MONGODB CONNECTION ERROR: ', err.message);
-    });
+    this.connect(url).then( _ => {
+      console.log("Established new connection with url", url);
+    }).catch(err => {
+      console.error('MONGODB ERROR: ', err.message);
+    })
+
+  }
+  async connect(url) {
+    try {
+      await mongoose.connect(url);
+    } catch (e) {
+      throw e;
+    }
   }
 }
 
