5c553842f6134f0e7dbb4764e4cf5371fbade3a3
Sunil Kumar Samanta <sunilthedj@gmail.com>
2020-10-07 16:43:14 +0530

    Helmet Added for better security

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const helmet = require('helmet');
 const server = express();
+server.use(helmet())
 const cors = require('cors');
 const corsOptions = {
     origin: "*"
